aa4b8121f6c027940364635273c559935e39d41b
Bruno Barange <bruno.akun@gmail.com>
2022-06-01 10:31:13 +0200

    Curso Angular. Interpolación. Vídeo 7

---
+++ src/app/empleado/empleado.component.ts
@@ -0,0 +1,28 @@
+import { getTestBed } from '@angular/core/testing';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-empleado',
+  templateUrl: './empleado.component.html',
+  styleUrls: ['./empleado.component.css']
+})
+export class EmpleadoComponent implements OnInit {
+
+  // ATRIBUTOS de la clase Empleado 
+  _nombre = 'Bruno';
+  _edad = 5;
+
+  constructor() {
+  }
+
+  // GETTERS Y SETTERS
+  get nombre(): string { return this._nombre; }
+  get edad(): number { return this._edad; }
+
+  // METODOS0
+
+  ngOnInit(): void {
+    console.log(this.edad);
+  }
+
+}
